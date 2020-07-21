0c14d46dcd14158119266a04a750b6e11679236d
Chris <chris.adonizio@gmail.com>
2020-07-21 16:37:14 -0400

    fix

--- logic.js
+++ logic.js
@@ -1,33 +1,16 @@
-
 var wordPool = ["apple", "bat", "door", "eggplant", "fork", "gate", "house", "ice", "jar", "kite", "lamp", "moon", "notebook", "orange", "pencil", "quilt", "robot", "socks", "tree", "umbrella", "vase", "window", "xylophone", "yellow", "zipper"]
-
-currentword = wordPool[Math.floor(Math.random()*wordPool.length)];
-
-
-console.log(currentword);
-
-
-console.log(userGuess);
+var playerKey = "";
+var keyPressed = "";
+var lives = 3;
+var score = 0;
+var currentWord = wordPool[Math.floor(Math.random()*wordPool.length)];
+// console.log(currentWord);
 //JQuery to detect a keypress
-$(document).keypress(function(event){
-    // Check if the key pressed is a letter.
+window.addEventListener("keyup", function(event){
     if (event.keyCode >= 65 && event.keyCode <= 90) {
         // Converts all key clicks to lowercase letters.
-        var letterGuessed = event.key.toLowerCase();
+        playerKey = event.key.toLowerCase();
         //adds guessed letter to the total guess
-        
+		document.getElementById("word").innerHTML = playerKey; 
         }
-
-
-
-    //when keycode = 13 the user pressed the enter key
-    // if(even.keycode == '13'){
-    //         if (userGuess == currentword){
-    //             console.log("correct");
-
-    //         }
-    //         else{
-    //             console.log("wrong");
-    //         }
-    //     }
-    });
+},false)
