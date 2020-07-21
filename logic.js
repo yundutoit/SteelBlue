8ba386263d5caf4aa822e61b69ff0c721fc7a3e3
Chris <chris.adonizio@gmail.com>
2020-07-21 17:05:47 -0400

    working text

--- logic.js
+++ logic.js
@@ -3,16 +3,23 @@ var playerKey = "";
 var keyPressed = "";
 var lives = 3;
 var score = 0;
-var totalGuess;
+var totalGuess= "";
 var currentWord = wordPool[Math.floor(Math.random()*wordPool.length)];
-// console.log(currentWord);
-//JQuery to detect a keypress
+// to detect a keypress
+document.getElementById("neededWord").innerHTML = currentWord;
 window.addEventListener("keyup", function(event){
     if (event.keyCode >= 65 && event.keyCode <= 90) {
         // Converts all key clicks to lowercase letters.
         playerKey = event.key.toLowerCase();
         //adds guessed letter to the total guess
+        if (currentWord.indexOf(playerKey)===totalGuess.length){
 		totalGuess = totalGuess+ "" +playerKey;
         }
+        else if (currentWord.lastIndexOf(playerKey)===totalGuess.length){
+            totalGuess = totalGuess+ "" +playerKey;
+            }
         document.getElementById("word").innerHTML = totalGuess; 
+    }
 },false)
+
+ 
