00ce9ab97f6d5a52867b4f473c7f679687f24eb5
Andra Marinescu <andra.marinescu@hotmail.com>
2020-07-21 12:05:33 -0700

    word list

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-var wordPool = ["cat", "profound", "zanny", ""]
+var wordPool = ["apple", "bat", "door", "eggplant", "fork", "gate", "house", "ice", "jar", "kite", "lamp", "moon", "notebook", "orange", "pencil", "quilt", "robot", "socks", "tree", "umbrella", "vase", "window", "xylophone", "yellow", "zipper"]
 
 currentword = wordPool[Math.floor(Math.random()*wordPool.length)];
 
