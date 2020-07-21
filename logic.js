6fd6a5acf9b19a7a8775f3aa300e1c882395305b
Chris <chris.adonizio@gmail.com>
2020-07-21 15:30:29 -0400

    userinput

--- logic.js
+++ logic.js
@@ -1,5 +1,33 @@
+//stores all the words to choose from
 var wordPool = ["cat", "profound", "zanny", ""]
-
+//generates random word in position 0- to last index of wordPool
 currentword = wordPool[Math.floor(Math.random()*wordPool.length)];
 
+
 console.log(currentword);
+
+
+console.log(userGuess);
+//JQuery to detect a keypress
+$(document).keypress(function(event){
+    // Check if the key pressed is a letter.
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        // Converts all key clicks to lowercase letters.
+        var letterGuessed = event.key.toLowerCase();
+        //adds guessed letter to the total guess
+        
+        }
+
+
+
+    //when keycode = 13 the user pressed the enter key
+    // if(even.keycode == '13'){
+    //         if (userGuess == currentword){
+    //             console.log("correct");
+
+    //         }
+    //         else{
+    //             console.log("wrong");
+    //         }
+    //     }
+    });
